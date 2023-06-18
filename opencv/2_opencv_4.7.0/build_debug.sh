81a190c871b63597142941252b3a4b00828476b7
davi <davi@tihoni.com>
2023-06-18 17:06:29 +0800

    enable JPEG

--- opencv/2_opencv_4.7.0/build_debug.sh
+++ opencv/2_opencv_4.7.0/build_debug.sh
@@ -6,11 +6,12 @@ cmake \
 	-DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
 	-DCMAKE_INSTALL_PREFIX=../install \
 	-DCMAKE_BUILD_TYPE=Debug \
-	-DBUILD_opencv_world=ON \
+	-DBUILD_opencv_world=OFF \
 	-DBUILD_SHARED_LIBS=ON \
 	-DBUILD_EXAMPLES=ON \
 	-DINSTALL_C_EXAMPLES=ON  \
 	-DINSTALL_PYTHON_EXAMPLES=ON \
+	-DBUILD_JPEG=ON \
 	../opencv
 
 VERBOSE=0 make -j30 && make install
